fa4025259ed875f0e5c2925b4357425af7f4a098
HigorDevJ <pedrohigor2004@gmail.com>
2022-06-05 14:56:02 -0300

    aa

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -2,6 +2,10 @@ const ContactArea = () => {
     return (  
         <div id="contact">
             <h1>Contato</h1>
+            <ul>
+                <a href=""></a>
+                <a href=""></a>
+            </ul>
         </div>
     );
 }
