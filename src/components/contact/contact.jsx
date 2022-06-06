9d0740dcc48e2558fe74d0ba6057488d6c0af5ed
HigorDevJ <pedrohigor2004@gmail.com>
2022-06-06 16:45:20 -0300

    add contactcss

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -1,11 +1,44 @@
+import github from './../../imgs/github.png'
+import endereço from './../../imgs/endereco.png'
+import instagram from './../../imgs/Instagram.png'
+import telegram from './../../imgs/telegram.png'
+
+import './contact.css'
+
 const ContactArea = () => {
-    return (  
-        <div id="contact">
-            <h1>Contato</h1>
-            <ul>
-            </ul>
-        </div>
+    return (
+        <footer>
+            <div id="contact" className='contact'>
+                <h1>Contact</h1>
+                <div className="i-contato">
+                    <ul>
+                        <li> 
+                            <img src={endereço} alt="endereço" />
+                            Brasil, São Paulo, Rio das Pedras
+                        </li>
+                    </ul>
+                    <ul>
+                        <li>
+                            <img src={github} alt="github" />
+                            <a href="https://github.com/HigorDevJ" target="_blank">Higor Dev</a>
+                        </li>
+                    </ul>
+                    <ul>
+                        <li>
+                            <img src={instagram} alt="Instagram" />
+                            <a href="https://www.instagram.com/higorxl.sz/?hl=pt-br" target="_blank">Higor</a>
+                        </li>
+                    </ul>
+                    <ul>
+                        <li>
+                            <img src={telegram} alt="telegram" />
+                            <a href="https://t.me/higorkk" target='_blank'>higorkk</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </footer>
     );
 }
- 
+
 export default ContactArea;
